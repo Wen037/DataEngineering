ceb92f65d890187e2a2ba1c10088876c3b9b9922
darrenchew123 <120630500+darrenchew123@users.noreply.github.com>
2023-08-13 18:35:43 +0800

    Update Q5.sql

--- SQL/Q5.sql
+++ SQL/Q5.sql
@@ -1,16 +1,12 @@
-SELECT product_id, product_name, COUNT(customer_id) AS re_subscriber_count
-FROM (
-  SELECT product_id,product_name, customer_id
-  FROM tblSubscriptionInfo
-  GROUP BY product_id, product_name, customer_id
-  HAVING COUNT(*) > 1
-) AS ReSubscriptions
-GROUP BY product_id, product_name;
+SELECT customer_id ,customer_name, product_id, product_name, COUNT(product_id)AS subscribe_count
+FROM tblSubscriptionInfo
+GROUP BY customer_id ,customer_name, product_id, product_name
+HAVING subscribe_count >=2;
 
 
 -- Output
--- +------------+--------------+---------------------+
--- | product_id | product_name | re_subscriber_count |
--- +------------+--------------+---------------------+
--- |        101 | Autocad 2022 |                   1 |
--- +------------+--------------+---------------------+
+--+-------------+---------------+------------+--------------+-----------------+
+--| customer_id | customer_name | product_id | product_name | subscribe_count |
+--+-------------+---------------+------------+--------------+-----------------+
+--|        1005 | Tom           |        101 | Autocad 2022 |               2 |
+--+-------------+---------------+------------+--------------+-----------------+
